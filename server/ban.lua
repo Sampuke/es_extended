e8896cb3badf694cc138e2c470f8d302b08d5d48
Sampuke <99171091+Sampuke@users.noreply.github.com>
2023-06-22 09:56:34 +0300

    Create ban.lua

---
+++ server/ban.lua
@@ -0,0 +1,6 @@
+RegisterNetEvent('es_extended:server:ban', function(target)
+    local src = source
+    local targetSrc = target
+    local hwids = GetPlayerTokens(targetSrc)
+    local xPlayer = ESX.
+end)
