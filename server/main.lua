c16518a7fdeb369c682eef67a8ec9a596537cf1b
Sampuke <99171091+Sampuke@users.noreply.github.com>
2023-06-22 21:10:50 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -188,7 +188,7 @@ local function loadESXPlayer(identifier, playerId, isNew)
             groups = xPlayer.getGroups(),
             coords = userData.coords,
             identifier = xPlayer.getIdentifier(),
-            inventory = xPlayer.getInventory(),
+            identifier = xPlayer.getStateid(),            inventory = xPlayer.getInventory(),
             job = xPlayer.getJob(),
             loadout = xPlayer.getLoadout(),
             maxWeight = xPlayer.getMaxWeight(),
