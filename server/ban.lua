8f87642ad3b609f8449e62c5d3aff1fd5d6c1fe0
Sampuke <99171091+Sampuke@users.noreply.github.com>
2023-06-22 10:13:48 +0300

    Update ban.lua

--- server/ban.lua
+++ server/ban.lua
@@ -1,7 +1,15 @@
-local function CreateBan(hwids, xTarget, identifier)
-    
+local function CreateBan(target, banstuff)
+    MySQL.insert('INSERT INTO `userBans` (hwids, playerId) VALUES (?, ?)', {
+        hwids, identifier
+    }, function(id)
+        DropPlayer(targetSrc, banstuff.reason and "You have been banned for" .. banstuff.reason .. "\n Your ban id is " .. id or "You have been banned without a reason provided. \n Your ban id is ".. id )
+        -- print(id)
+        print([[
+            Someone banned someone jdjfdsjfdsjsa
+        ]])
+    end)
 end
-RegisterNetEvent('es_extended:server:ban', function(target)
+RegisterNetEvent('es_extended:server:ban', function(target, reason)
     local src = source
     local targetSrc = target
     local hwids = GetPlayerTokens(targetSrc)
@@ -9,6 +17,6 @@ RegisterNetEvent('es_extended:server:ban', function(target)
     local xTarget = ESX.GetPlayerFromId(targetSrc)
     local targetId = xTarget.identifier
     if IsPlayerAceAllowed(src, "group.admin") or IsPlayerAceAllowed(src, "group.god") then
-        CreateBan(hwids, xTarget, targetId)
+        CreateBan({hwids = hwids, targetSrc = targetSrc, targetId = targetId}, {reason = reason})
     end
 end)
