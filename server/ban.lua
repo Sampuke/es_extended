74bc833c770f5f53e0ca4df27a3f858cf1669d43
Sampuke <99171091+Sampuke@users.noreply.github.com>
2023-06-22 10:04:34 +0300

    Update ban.lua

--- server/ban.lua
+++ server/ban.lua
@@ -1,6 +1,14 @@
+local function CreateBan(hwids, xTarget, identifier)
+    
+end
 RegisterNetEvent('es_extended:server:ban', function(target)
     local src = source
     local targetSrc = target
     local hwids = GetPlayerTokens(targetSrc)
-    local xPlayer = ESX.
+    local xPlayer = ESX.GetPlayerFromId(src)
+    local xTarget = ESX.GetPlayerFromId(targetSrc)
+    local targetId = xTarget.identifier
+    if IsPlayerAceAllowed(src, "group.admin") or IsPlayerAceAllowed(src, "group.god") then
+        CreateBan(hwids, xTarget, targetId)
+    end
 end)
